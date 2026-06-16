af70f591130ebd33526d183c578f02924c2a8d4e
Rusil Varu <rusilvaru555@gmail.com>
2026-06-16 11:51:53 +0530

    new config try

--- api/test.js
+++ api/test.js
@@ -1,24 +1,34 @@
 export default async function handler(req, res) {
+
   try {
 
     const response = await fetch(
-      "https://mdm-apraava.pgvcl.com/login"
+      "https://mdm-apraava.pgvcl.com/login",
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        }
+      }
     );
 
-    const html = await response.text();
+    const text = await response.text();
 
     res.json({
       success: true,
       status: response.status,
-      length: html.length
+      length: text.length
     });
 
   } catch (err) {
 
     res.status(500).json({
       success: false,
-      error: String(err)
+      name: err.name,
+      message: err.message,
+      cause: String(err.cause)
     });
 
   }
+
 }
